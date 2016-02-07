5bc2e4e8ccf41d5cdeee51b7ef5d333a91762bad
Jinhuan Li <likers.li33@gmail.com>
2016-02-06 19:14:03 -0600

    finish allsubsets

--- DP/AllSubsets.py
+++ DP/AllSubsets.py
@@ -1,8 +1,24 @@
+from copy import deepcopy
 class Solution:
 	def getSubsets(self, parentSet):
 		print parentSet
+		result = [[]]
+		for item in parentSet:
+			holder = deepcopy(result)
+			for set in holder:
+				set.append(item)
+				result.append(set)
+			
+		print result
+
+
+
+
+
+
+
 
 so = Solution()
-so.getSubsets([1,2,3,4,5,6])
+so.getSubsets([1,2,3])
 
 
